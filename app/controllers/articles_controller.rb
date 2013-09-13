a9a1dbd86f1c5c0746969e01ce756753923a4423
Rahul <techniciablog@gmail.com>
2013-09-13 22:43:08 +0530

    Added flash messages plus several other features

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -8,4 +8,43 @@ class ArticlesController < ApplicationController
 	def show
 		@article = Article.find(params[:id])
 	end
+
+	def new
+		@article = Article.new
+	end
+
+	def create
+		@article = Article.new(article_params)
+		@article.save
+
+		flash.notice = "Article '#{@article.title}' Created!"
+
+		redirect_to article_path(@article)
+	end
+
+	def article_params
+		params.require(:article).permit(:title, :body)
+	end
+
+	def destroy
+		@article = Article.find(params[:id])
+		@article.destroy
+
+		flash.notice = "Article '#{@article.title}' Destroyed!"
+
+		redirect_to '/articles'
+	end
+
+	def edit
+		@article = Article.find(params[:id])
+	end
+
+	def update
+		@article = Article.find(params[:id])
+		@article.update(article_params)
+
+		flash.notice = "Article '#{@article.title}' Updated!"
+
+		redirect_to article_path(@article)
+	end
 end
