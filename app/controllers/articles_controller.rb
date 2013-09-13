feb7b160c03e85164adf9f106016ab7a5bd3d574
Rahul <techniciablog@gmail.com>
2013-09-13 17:42:45 +0530

    Improved style

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -4,4 +4,8 @@ class ArticlesController < ApplicationController
 		# should be promoted to an instance variable.
 		@articles = Article.all
 	end
+
+	def show
+		@article = Article.find(params[:id])
+	end
 end
